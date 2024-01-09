b1481f7371f5426b1dbcbfee074d081970ef1784
nh52536 <92516889+nh52536@users.noreply.github.com>
2024-01-09 17:18:26 +0100

    Add files via upload

---
+++ back/src/main/java/com/example/drumreback/DrumreBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.drumreback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrumreBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrumreBackApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/example/drumreback/DrumreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drumreback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrumreBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
